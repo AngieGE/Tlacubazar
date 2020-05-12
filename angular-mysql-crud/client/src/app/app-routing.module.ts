bf36cda3266f851f3d8f0cf2ccc7fb5df003c89f
epicLevi <tomas.levi@gmail.com>
2020-05-11 21:12:32 -0500

    Added routes to app-routing module

--- angular-mysql-crud/client/src/app/app-routing.module.ts
+++ angular-mysql-crud/client/src/app/app-routing.module.ts
@@ -1,16 +1,61 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { StartComponent } from './components/start/start.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
+import { StoreDetailsComponent } from './components/store-details/store-details.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ClientCourseComponent } from './components/client-course/client-course.component';
+import { VendorCourseComponent } from './components/vendor-course/vendor-course.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ManageStoreComponent } from './components/manage-store/manage-store.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
+    component: StartComponent
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'catalogue',
+    component: CatalogueComponent
+  },
+  {
+    path: 'store',
+    component: StoreDetailsComponent
+  },
+  {
+    path: 'search',
+    component: SearchResultsComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'course',
+    component: ClientCourseComponent
+  },
+  {
+    path: 'vendor-course',
+    component: VendorCourseComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'manage-store',
+    component: ManageStoreComponent
   },
 
 ];
