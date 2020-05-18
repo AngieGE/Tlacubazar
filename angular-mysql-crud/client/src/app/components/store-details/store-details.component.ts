39984cd8489506bf6ecd1888b5a0a7eef6254835
EnriqueF <kike.fvla@gmail.com>
2020-05-18 12:26:41 -0500

    profile 90% listo, store details 60% listo

--- angular-mysql-crud/client/src/app/components/store-details/store-details.component.ts
+++ angular-mysql-crud/client/src/app/components/store-details/store-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TlacuServices } from '../../services/index';
+import { User } from 'src/app/models/User';
+import { Store } from 'src/app/models/Store';
+import { SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-store-details',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  socialUser: SocialUser;
+  stores: Store[];
+  constructor(
+    private tlacu: TlacuServices
+  ) {
+    this.user = this.tlacu.manager.user;
+    this.socialUser = this.tlacu.manager.socialUser;
+  }
 
   ngOnInit(): void {
+    this.tlacu.store.listStore(null, null, null, this.user.idUser, null).subscribe(
+      res => {
+        this.stores = res.body;
+        console.log(this.stores);
+      });
   }
 
 }
