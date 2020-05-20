1f956851a5c78122cc6016fd20d79834f267a763
unknown <a01421467@itesm.mx>
2020-05-20 02:06:04 -0500

    started /start page fixed

--- angular-mysql-crud/client/src/app/components/start/start.component.ts
+++ angular-mysql-crud/client/src/app/components/start/start.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
+import { AuthService, SocialUser } from 'angularx-social-login';
+import {TlacuServices} from '../../services/index';
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private tlacu: TlacuServices, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.tlacu.manager.unsetItems();
+    this.authService.signOut().then( res => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
